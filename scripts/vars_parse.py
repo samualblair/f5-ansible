5365e82fdd15e0987fe89881c50438b29982f39a
samualblair <10884846+samualblair@users.noreply.github.com>
2023-12-22 16:15:55 -0800

    Added UUID generation and schema version

--- scripts/vars_parse.py
+++ scripts/vars_parse.py
@@ -2,6 +2,7 @@
 # Simple Python Script to parse CSV file with headers into python device variables
 
 import csv
+import uuid
 
 with open('./variables/device_variables.csv', 'r') as csv_file:
     reader = csv.reader(csv_file)
@@ -15,12 +16,15 @@ with open('./variables/device_variables.csv', 'r') as csv_file:
     ansible_vars = { 'devices': [] }
     itterations = len(list_of_csv_lines)
     # Track first itteration, as headers of CSV should not be added to vars
-    first_itteration = 0
+    itteration_index_value = 0
 
     for list in list_of_csv_lines:
-        if first_itteration >= 1:
+        if itteration_index_value >= 1:
             value_setting = 0
             unit_dictionary = {}
+            # Generate and add a UUID
+            unit_dictionary['generated_uuid'] = str(uuid.uuid4())
+            # Parse each item and add to value with key of csv header name
             for item in list:
                 unit_dictionary[list_of_csv_lines[0][value_setting]] = item
                 value_setting += 1
@@ -29,7 +33,7 @@ with open('./variables/device_variables.csv', 'r') as csv_file:
             working_list.append(working_unit)
         # Catch on first itteration, that would be headers line in CSV
         else:
-            first_itteration = first_itteration + 1
+            itteration_index_value = itteration_index_value + 1
 
     print(ansible_vars['devices'])
     
